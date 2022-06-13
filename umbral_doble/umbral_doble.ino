66d9aee2bec812bda62422f1cee54eb7b6dc50b9
JaoRamos <bjrcomputacion@gmail.com>
2022-06-13 03:47:25 -0300

    varios

--- umbral_doble/umbral_doble.ino
+++ umbral_doble/umbral_doble.ino
@@ -1,6 +1,8 @@
-#define UMBRAL_ENCENDIDO 500;
-#define UMBRAL_APAGADO 600;
+#define UMBRAL_ENCENDIDO 615
+#define UMBRAL_APAGADO 410
 #define PIN_SALIDA 8
+#define ENCENDIDO 1
+#define APAGADO 0
 int estado_activacion = 0;
 
 void setup() {
@@ -9,10 +11,10 @@ void setup() {
 
 void loop() {
   int valor = analogRead(A0);
-  
-  if((valor < UMBRAL_APAGADO) && (estado_activacion==0)) estado_activacion = 1;
-  if((valor > UMBRAL_ENCENDIDO) && (estado_activacion==1)) estado_activacion = 0;
-
+  if((valor < UMBRAL_APAGADO) && (estado_activacion==ENCENDIDO)) 
+    estado_activacion = APAGADO;
+  if((valor > UMBRAL_ENCENDIDO) && (estado_activacion==APAGADO)) 
+    estado_activacion = ENCENDIDO;
   digitalWrite(PIN_SALIDA, estado_activacion);
 }
 
